a46b3ad93a76332d11c898a724a41d9805eed1ac
Elijah <velezelijah@gmail.com>
2016-05-28 17:04:51 +0000

    Restore via nyot updater. Tag: session20-start

--- routes.js
+++ routes.js
@@ -1,18 +1,21 @@
-// Local deps
+// Local dependencies
 var controllers = require('./controllers');
 
 var Router = {
-    
+
     init: function(app) {
-    
+
         // Home page
         app.get('/', controllers.Home.index);
-    
-    //About page
-    app.get('/about',controllers.About.index);
-//Users
+
+        // About page
+        app.get('/about', controllers.About.index);
+
+        // Users
         app.get('/users', controllers.Users.index);
         
+        // Search
+        app.get('/search', controllers.Search.index);
     }
 
 };
